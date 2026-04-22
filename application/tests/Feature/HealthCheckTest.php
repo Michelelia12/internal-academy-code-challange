98fcdb8e6ade334ca23da01208b3c032204d5dc6
Michele WSL <michele.elia12@gmail.com>
2026-04-22 07:50:26 +0000

    refactor(tests): align HealthCheckTest with #[Test] attribute convention

--- application/tests/Feature/HealthCheckTest.php
+++ application/tests/Feature/HealthCheckTest.php
@@ -5,15 +5,14 @@ declare(strict_types=1);
 namespace Tests\Feature;
 
 use PHPUnit\Framework\Attributes\CoversNothing;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
 #[CoversNothing]
 class HealthCheckTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
+    #[Test]
+    public function the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
 
